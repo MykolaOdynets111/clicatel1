a976f878480e1a09280844350e3a4ed4b7816f22
PERFECTIAL1\kmakohoniuk <kostiantyn.makohoniuk@perfectial.com>
2017-01-18 14:20:45 +0200

    add new tenant id for card test cases

--- touch-rest-api-tests/src/test/java/com/touch/tests/ChatsTests.java
+++ touch-rest-api-tests/src/test/java/com/touch/tests/ChatsTests.java
@@ -23,15 +23,19 @@ public class ChatsTests extends BaseTestClass {
     @Test
     public void getNewChatRoom() {
         String tenantWithBot = getTestTenant1().getId();
-        ChatRoomResponse chatRoom = chatsActions.getChatRoom(tenantWithBot, "testclient1@clickatelllabs.com", "test1","Android", token).as(ChatRoomResponse.class);
+        ChatRoomResponse chatRoom = chatsActions.getChatRoom(tenantWithBot, "testclient1@clickatelllabs.com", "test1", "Android", token).as(ChatRoomResponse.class);
         Assert.assertTrue(chatRoom.getChatroomJid().matches(".{32}@muc.clickatelllabs.com"));
 
     }
+
     @Test(dataProvider = "chatRoomsParameters")
     public void tryToGetNewChatRoomWithDifferentData(String tenantId, String clientJid, String clientId, int statusCode) {
-        Assert.assertEquals(chatsActions.getChatRoom(tenantId, clientJid, clientId,"Android", token).getStatusCode(),statusCode);
+        if (tenantId.equals("correct"))
+            tenantId = getTestTenant1().getId();
+        Assert.assertEquals(chatsActions.getChatRoom(tenantId, clientJid, clientId, "Android", token).getStatusCode(), statusCode);
 
     }
+
     @Test
     public void getNewSessionAndSessionList() {
         String sessionId = "testSession" + StringUtils.generateRandomString(10);
@@ -57,7 +61,7 @@ public class ChatsTests extends BaseTestClass {
         String sessionId = "wrong_sessionId";
         String expectedMessage = "ChatSession with id " + sessionId + " not found";
 //        Verify error message which we get when we try to get session data with wrong sessionId
-        Assert.assertTrue(chatsActions.getSession(sessionId,token).as(ErrorMessage.class).getErrorMessage().matches(expectedMessage));
+        Assert.assertTrue(chatsActions.getSession(sessionId, token).as(ErrorMessage.class).getErrorMessage().matches(expectedMessage));
 
     }
 
@@ -65,19 +69,20 @@ public class ChatsTests extends BaseTestClass {
     @Test
     public void getNewChatRoomWithWrongData() {
         // verify error message when we try to get chat room for not existing tenant
-        Assert.assertTrue(chatsActions.getChatRoom("wrong_tenantId", "testclient1@clickatelllabs.com", "test1","Android",token).as(ErrorMessage.class).getErrorMessage().matches("Tenant with id .+ not found"));
+        Assert.assertTrue(chatsActions.getChatRoom("wrong_tenantId", "testclient1@clickatelllabs.com", "test1", "Android", token).as(ErrorMessage.class).getErrorMessage().matches("Tenant with id .+ not found"));
     }
+
     @DataProvider
     private static Object[][] chatRoomsParameters() {
         return new Object[][]{
                 {"", "", "", 400},
-                {"20a9c80d53fb11e6a0280626baf6c11d", "", "", 400},
-                {"20a9c80d53fb11e6a0280626baf6c11d", "", "test1", 400},
+                {"correct", "", "", 400},
+                {"correct", "", "test1", 400},
                 {"test1", "testclient1@clickatelllabs.com", "test1", 404},
-                {"20a9c80d53fb11e6a0280626baf6c11d", "testclient1@clickatelllabs.com", "", 400},
-                {"20a9c80d53fb11e6a0280626baf6c11d", "test1", "test1", 200},
+                {"correct", "testclient1@clickatelllabs.com", "", 400},
+                {"correct", "test1", "test1", 200},
                 {"test1", "test1", "test1", 404},
-                {"20a9c80d53fb11e6a0280626baf6c11d", "testclient1@", "test1", 200}
+                {"correct", "testclient1@", "test1", 200}
         };
     }
 
