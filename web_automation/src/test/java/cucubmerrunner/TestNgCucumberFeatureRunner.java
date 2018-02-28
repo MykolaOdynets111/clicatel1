8e144117a29a57baf96c08e045e459cc0fc58a9a
tmytlovych <tarasmytlovych@gmail.com>
2018-02-28 17:43:58 +0200

    Draft commit

--- web_automation/src/test/java/cucubmerrunner/TestNgCucumberFeatureRunner.java
+++ web_automation/src/test/java/cucubmerrunner/TestNgCucumberFeatureRunner.java
@@ -1,30 +1,76 @@
+//package cucubmerrunner;
+//
+//import cucumber.api.testng.CucumberFeatureWrapper;
+//import cucumber.api.testng.CucumberFeatureWrapperImpl;
+//import cucumber.api.testng.TestNGCucumberRunner;
+//import cucumber.runtime.model.CucumberFeature;
+//import driverManager.DriverFactory;
+//import org.testng.annotations.AfterClass;
+//import org.testng.annotations.BeforeClass;
+//import org.testng.annotations.Test;
+//
+//
+//public class TestNgCucumberFeatureRunner{
+//
+//    private TestNGCucumberRunner testNGCucumberRunner;
+//    private CucumberFeatureWrapper cucumberFeature;
+//
+//    public TestNgCucumberFeatureRunner(CucumberFeature cucumberFeature2, Object runner) {
+//        testNGCucumberRunner = new TestNGCucumberRunner(runner.getClass());
+//        cucumberFeature = new CucumberFeatureWrapperImpl(cucumberFeature2);
+//    }
+//
+////    @BeforeClass(alwaysRun = true)
+////    public void startContainer(){
+////        DriverFactory.openUrl();
+////        System.out.println("!!!!!!!!!!!!!!!Cucumber feature :" + cucumberFeature.toString());
+////    }
+//
+//    @Test(groups = {"cucumber"})
+//    public void feature() {
+//        this.testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+//    }
+//
+//    @AfterClass(alwaysRun = true)
+//    public void tearDownClass(){
+////        DriverFactory.closeBrowser();
+////        DriverFactory.closeSecondBrowser();
+////        this.testNGCucumberRunner.finish();
+//    }
+//
+//}
 package cucubmerrunner;
 
 import cucumber.api.testng.CucumberFeatureWrapper;
 import cucumber.api.testng.CucumberFeatureWrapperImpl;
 import cucumber.api.testng.TestNGCucumberRunner;
 import cucumber.runtime.model.CucumberFeature;
-import driverManager.DriverFactory;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Factory;
 import org.testng.annotations.Test;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestNgCucumberFeatureRunner{
 
     private TestNGCucumberRunner testNGCucumberRunner;
     private CucumberFeatureWrapper cucumberFeature;
 
+    public TestNgCucumberFeatureRunner(){}
+
     public TestNgCucumberFeatureRunner(CucumberFeature cucumberFeature2, Object runner) {
         testNGCucumberRunner = new TestNGCucumberRunner(runner.getClass());
-        cucumberFeature = new CucumberFeatureWrapperImpl(cucumberFeature2);
-    }
+        cucumberFeature = new CucumberFeatureWrapperImpl(cucumberFeature2);}
 
-//    @BeforeClass(alwaysRun = true)
-//    public void startContainer(){
-//        DriverFactory.openUrl();
-//        System.out.println("!!!!!!!!!!!!!!!Cucumber feature :" + cucumberFeature.toString());
-//    }
+        @Factory
+        public Object[] features() {
+        List objects = new ArrayList<>();
+        TestNGCucumberRunner testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+        for (CucumberFeature testDatum : testNGCucumberRunner.getFeatures()) {
+            objects.add(new TestNgCucumberFeatureRunner(testDatum, this));
+        }
+            return objects.toArray();
+    }
 
     @Test(groups = {"cucumber"})
     public void feature() {
@@ -32,10 +78,6 @@ public class TestNgCucumberFeatureRunner{
     }
 
     @AfterClass(alwaysRun = true)
-    public void tearDownClass(){
-//        DriverFactory.closeBrowser();
-//        DriverFactory.closeSecondBrowser();
-//        this.testNGCucumberRunner.finish();
+    public void tearDownClass(){this.testNGCucumberRunner.finish();
     }
-
 }
--- web_automation/src/test/java/runner/RunGenBankTest.java
+++ web_automation/src/test/java/runner/RunGenBankTest.java
@@ -1,8 +1,10 @@
 package runner;
 
+import cucubmerrunner.TestNgCucumberFeatureRunner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.CucumberFeatureWrapper;
 import cucumber.api.testng.TestNGCucumberRunner;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
@@ -19,27 +21,28 @@ import org.testng.annotations.Test;
         features ="src/test/java/scenario/general_bank_demo",
         glue ="steps")
 //public class RunGenBankTest extends SuiteCucumberRunner {
-public class RunGenBankTest {
-    private TestNGCucumberRunner testNGCucumberRunner;
-
-    @BeforeClass(alwaysRun = true)
-    public void setUpClass() throws Exception {
-        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
-        }
-
-    @Test(description = "Runs Cucumber Feature", dataProvider = "features")
-    public void mainFlowFeature(CucumberFeatureWrapper cucumberFeature) {
-        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
-        }
-
-    @DataProvider
-    public Object[][] features() {
-        return testNGCucumberRunner.provideFeatures();
-        }
-
-    @AfterClass(alwaysRun = true)
-    public void tearDownClass() throws Exception {
-        testNGCucumberRunner.finish();
-
-        }
+//public class RunGenBankTest {
+public class RunGebBankTest extends TestNgCucumberFeatureRunner{
+//    private TestNGCucumberRunner testNGCucumberRunner;
+//
+//    @BeforeClass(alwaysRun = true)
+//    public void setUpClass(ITestContext context) throws Exception {
+//        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+//        }
+//
+//    @Test(description = "Runs Cucumber Feature", dataProvider = "features")
+//    public void mainFlowFeature(CucumberFeatureWrapper cucumberFeature) {
+//        testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+//        }
+//
+//    @DataProvider(parallel = true)
+//    public Object[][] features() {
+//        return testNGCucumberRunner.provideFeatures();
+//        }
+//
+//    @AfterClass(alwaysRun = true)
+//    public void tearDownClass() throws Exception {
+//        testNGCucumberRunner.finish();
+//
+//        }
 }
