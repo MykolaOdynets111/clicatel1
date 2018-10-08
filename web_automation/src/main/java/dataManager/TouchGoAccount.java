abedd0c571545c2406f1490ee610a6cff068960a
tmytlovych <tarasmytlovych@gmail.com>
2018-10-08 11:46:15 +0300

    Updated logic for skipping the rest tests after widget connection failure

---
+++ web_automation/src/main/java/dataManager/TouchGoAccount.java
@@ -0,0 +1,18 @@
+//package dataManager;
+//
+//public enum TouchGoAccount {
+//
+//    QA_ACCOUNT("");
+//
+//    String email;
+//    String pass;
+//    String tenantOrgName;
+//    String env;
+//
+//    TouchGoAccount(String email, String pass, String tenantOrgName, String env) {
+//        this.email = email;
+//        this.pass = pass;
+//        this.tenantOrgName = tenantOrgName;
+//        this.env = env;
+//    }
+//}
