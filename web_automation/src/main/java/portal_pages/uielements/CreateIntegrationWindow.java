00186e898cd058e4934a11a610d4c282b5c2ebac
tmytlovych <tarasmytlovych@gmail.com>
2018-12-10 16:18:31 +0200

    Increased timeout

--- web_automation/src/main/java/portal_pages/uielements/CreateIntegrationWindow.java
+++ web_automation/src/main/java/portal_pages/uielements/CreateIntegrationWindow.java
@@ -19,7 +19,7 @@ public class CreateIntegrationWindow extends BasePortalWindow {
 
     public void setUpFBIntegration(String fbPage){
         try {
-            Thread.sleep(5000);
+            Thread.sleep(15000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
