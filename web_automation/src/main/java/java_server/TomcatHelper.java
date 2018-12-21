fac8189c2c5d644ff1397c7c3ff92115bcdd9ac1
tmytlovych <tarasmytlovych@gmail.com>
2018-12-21 13:12:21 +0200

    Continue working

--- web_automation/src/main/java/java_server/TomcatHelper.java
+++ web_automation/src/main/java/java_server/TomcatHelper.java
@@ -1,24 +1,45 @@
-//package java_server;
-//
-//import org.apache.catalina.Context;
-//import org.apache.catalina.startup.Tomcat;
-//
-//import java.io.File;
-//
-//public class TomcatHelper {
-//
-//    private Tomcat tomcat;
-//
-//    public Tomcat getTomcat(){
-//        tomcat = new Tomcat();
-//        tomcat.setPort(8080);
-//        tomcat.setHostname("localhost");
-//        String appBase = ".";
-//        tomcat.getHost().setAppBase(appBase);
-//        File docBase = new File(System.getProperty("java.io.tmpdir"));
-//        Context context = tomcat.addContext("", docBase.getAbsolutePath());
-//        return tomcat;
-//    }
-//
-//    public static void main
-//}
+package java_server;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+
+import java.io.File;
+
+public class TomcatHelper {
+
+    private Tomcat tomcat;
+
+    public Tomcat getTomcat(){
+        tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.setHostname("localhost");
+        String appBase = ".";
+        tomcat.getHost().setAppBase(appBase);
+        File docBase = new File(System.getProperty("java.io.tmpdir"));
+        Context context = tomcat.addContext("", docBase.getAbsolutePath());
+        return tomcat;
+    }
+
+    public static void main(String[] args){
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.setHostname("localhost");
+//        File appDir = new File(System.getProperty("user.dir")+"/src/main/java/java_server/");
+//        // app dir is relative to server home
+//        StandardContext ctx = (StandardContext) tomcat.addWebapp(
+//                null, "/automationcallback", appDir.getAbsolutePath());
+        String appBase = ".";
+        tomcat.getHost().setAppBase(appBase);
+        File docBase = new File(System.getProperty("java.io.tmpdir"));
+        tomcat.addContext("", docBase.getAbsolutePath());
+        try {
+            tomcat.start();
+        } catch (LifecycleException e) {
+            e.printStackTrace();
+        }
+        tomcat.getServer().await();
+        int a =2;
+    }
+}
--- web_automation/src/main/java/java_server/WebSocketServer.java
+++ web_automation/src/main/java/java_server/WebSocketServer.java
@@ -1,4 +1,5 @@
 package java_server;
+import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
@@ -10,5 +11,10 @@ public class WebSocketServer {
     @OnMessage
     public void onMessage(Session session, String message) {
         System.out.println("onMessage::From=" + session.getId() + " Message=" + message);
+    }
+
+    @OnError
+    public void onError(Session session) {
+        System.out.println("onMessage::From=" + session.getId() + " Message= + connection session error");
     }
 }
