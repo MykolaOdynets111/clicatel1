867eb5efc5223917a6f275b0d98c2c78185a3445
tmytlovych <tarasmytlovych@gmail.com>
2019-02-14 12:21:11 +0200

    Updated file path

--- web_automation/src/test/java/steps/BasePortalSteps.java
+++ web_automation/src/test/java/steps/BasePortalSteps.java
@@ -642,7 +642,8 @@ public class BasePortalSteps {
 //        String basePath = System.getProperty("user.dir");
 //        if(ConfigManager.isRemote()) basePath = basePath.replace("web_automation", "Workspace/web_automation");
 //        if(photoStrategy.equals("new photo")) photoPath = basePath + "/src/test/resources/agentphoto/agent_photo.png";
-        Assert.assertTrue(new File("/src/test/resources/agentphoto/agent_photo.png").exists());
+        Assert.assertTrue(new File("/src/test/resources/generalbank/tie.xml").exists(),
+                "File not found");
         portalUserManagementThreadLocal.get().uploadPhoto("/src/test/resources/agentphoto/agent_photo.png");
     }
 
