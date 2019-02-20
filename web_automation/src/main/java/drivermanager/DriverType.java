07d86a364e8497c119a88340d4089938b7ac25d5
tmytlovych <tarasmytlovych@gmail.com>
2019-02-20 14:40:14 +0200

    Added incognito chrome option (fot test)

--- web_automation/src/main/java/drivermanager/DriverType.java
+++ web_automation/src/main/java/drivermanager/DriverType.java
@@ -34,6 +34,7 @@ public enum DriverType {
             options.addArguments("no-proxy-server");
             options.addArguments("no-sandbox");
             options.addArguments("disable-notifications");
+            options.addArguments("incognito");
             return options;
 		}
 
