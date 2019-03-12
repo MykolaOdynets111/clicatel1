062c6b5c0129e842d8d740eab933e2dc0906829c
tmytlovych <tarasmytlovych@gmail.com>
2019-03-12 16:08:32 +0200

    Updated remote url for demo1

--- web_automation/src/main/java/drivermanager/DriverFactory.java
+++ web_automation/src/main/java/drivermanager/DriverFactory.java
@@ -141,8 +141,7 @@ public class DriverFactory {
                 Assert.assertTrue(false, "!!! Incorrect remote driver is used. ");
             }
             if(ConfigManager.getEnv().equalsIgnoreCase("testing")||
-                    ConfigManager.getEnv().equalsIgnoreCase("dev") ||
-                    ConfigManager.getEnv().equalsIgnoreCase("demo1")){
+                    ConfigManager.getEnv().equalsIgnoreCase("dev")){
                 REMOTE_URL = "http://selenium.clickatelllabs.com:4444/wd/hub";
             }
             RemoteWebDriver remoteWebDriver = new RemoteWebDriver(new URL(REMOTE_URL), capabilities);
