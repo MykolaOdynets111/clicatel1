cfc7fce5cf4359f74230d2e88c95209024671ce7
tmytlovych <tarasmytlovych@gmail.com>
2019-04-10 12:38:28 +0300

    Removed some unused lines

--- web_automation/src/main/java/javaserver/Server.java
+++ web_automation/src/main/java/javaserver/Server.java
@@ -36,7 +36,7 @@ public class Server {
             return "http://" + Server.INTERNAL_CI_IP + ":" + Server.SERVER_PORT;
         }else{
             // to provide local ngrok url
-            return "http://61bec137.ngrok.io";
+            return "http://bc77f311.ngrok.io";
         }
     }
 
