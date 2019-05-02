40b1ddacd473cd69608a7448a53d1f589cf35025
tarasmytlovych <tarasmytlovych@gmail.com>
2019-05-02 11:03:04 +0300

    Updated touch go runner (removed deprecated usages)

--- web_automation/src/test/java/runner/touchgo/RunTouchGoCreatingAccountTest.java
+++ web_automation/src/test/java/runner/touchgo/RunTouchGoCreatingAccountTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Test(groups = {"touchgo"})
 @CucumberOptions(
-        format={"com.github.kirlionik.cucumberallure.AllureReporter"
+        plugin={"com.github.kirlionik.cucumberallure.AllureReporter"
         },
         monochrome = true,
         features ="src/test/java/scenario/touchgo/newaccount/SignUpNewAccount.feature/",
