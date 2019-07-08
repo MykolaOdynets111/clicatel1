b2300c66034c3682a4f043d6ab970bf18d8a54d5
tarasmytlovych <tarasmytlovych@gmail.com>
2019-07-08 16:51:43 +0300

    Removed commented out unused code

--- web_automation/src/main/java/emailhelper/GmailProperties.java
+++ web_automation/src/main/java/emailhelper/GmailProperties.java
@@ -8,11 +8,6 @@ public class GmailProperties {
 
         public static Properties configureProperties(String host){
         Properties properties = new Properties();
-//        try {
-//            properties.load(new FileInputStream("src/main/resources/project.properties"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
         properties.put("mail.imap.host", host);
         properties.put("mail.imap.port", "587");
         properties.put("mail.imap.auth", "true");
