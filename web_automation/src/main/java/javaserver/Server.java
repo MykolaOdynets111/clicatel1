5ab5166bcd35bfdddeec8f68a592032a49f40377
serhiiperfectial <45171590+serhiiperfectial@users.noreply.github.com>
2019-07-11 13:49:13 +0300

    TPLAT-4272 (#56)
    TPLAT-4272 (#56)

--- web_automation/src/main/java/javaserver/Server.java
+++ web_automation/src/main/java/javaserver/Server.java
@@ -36,7 +36,7 @@ public class Server {
             return "http://" + Server.INTERNAL_CI_IP + ":" + Server.SERVER_PORT;
         }else{
             // to provide local ngrok url
-            return "http://45cfff2e.ngrok.io";
+            return "http://ae8c6459.ngrok.io";
         }
     }
 
