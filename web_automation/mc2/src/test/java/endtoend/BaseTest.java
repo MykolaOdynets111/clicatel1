ad84068e3e85b09e6af9711c1052cf0c6b5a6f8e
tarasmytlovych <tarasmytlovych@gmail.com>
2019-07-25 16:42:24 +0300

    Added updating run environment after mc2 test for reporting

--- web_automation/mc2/src/test/java/endtoend/BaseTest.java
+++ web_automation/mc2/src/test/java/endtoend/BaseTest.java
@@ -1,13 +1,33 @@
 package endtoend;
 
+import drivermanager.ConfigManager;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 @Test
 public class BaseTest {
 
     @AfterSuite(alwaysRun = true)
     public void setUpEnvsProp(){
+        try {
+            FileInputStream in = new FileInputStream("src/test/resources/allureconfigs/environment.properties");
+            Properties props = new Properties();
+            props.load(in);
+            in.close();
 
+            FileOutputStream out = new FileOutputStream("src/test/resources/allureconfigs/environment.properties");
+            props.setProperty("Browser", ConfigManager.getDriverType().name());
+            props.setProperty("Env", ConfigManager.getEnv());
+
+            props.store(out, null);
+            out.close();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
