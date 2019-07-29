bd0eb64eca64606e630ccf43ef4c1ae3b3c1acb4
tarasmytlovych <tarasmytlovych@gmail.com>
2019-07-29 15:20:00 +0300

    Added screenshots capturing for mc2 reports

--- web_automation/mc2/src/test/java/endtoend/FirstTest.java
+++ web_automation/mc2/src/test/java/endtoend/FirstTest.java
@@ -2,10 +2,15 @@ package endtoend;
 
 import driverfactory.MC2DriverFactory;
 import io.qameta.allure.Description;
+import io.qameta.allure.Link;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import listeners.TestAllureListener;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import portalpages.PortalSignUpPage;
 
+@Listeners({TestAllureListener.class})
 @Test(testName = "First Test")
 public class FirstTest extends BaseTest {
 
@@ -20,6 +25,8 @@ public class FirstTest extends BaseTest {
 
     @Description("The very first test")
     @Test(testName = "Test with only names") //test name ignored on method level
+    @Severity(SeverityLevel.CRITICAL)
+    @Link("https://jira.clickatell.com/browse/TPORT-9659")
     public void onlyNames(){
         signUpPage.setFirstName("Tom Jones");
         signUpPage.setAccountName("allure");
@@ -28,13 +35,11 @@ public class FirstTest extends BaseTest {
 
     @Description("The very second test")
     @Test(testName = "Test with full sign up data") //test name ignored on method level
+    @Severity(SeverityLevel.BLOCKER)
+    @Link("https://jira.clickatell.com/browse/TPORT-9659")
     public void allInfo(){
         signUpPage.signUp("Tom Jones", "alluew2", "aqa@a.a", "123456");
         Assert.assertEquals(1, 1, "equals");
     }
 
-    @AfterMethod(alwaysRun = true)
-    public void tearDown(){
-        MC2DriverFactory.closePortalBrowser();
-    }
 }
---
+++ web_automation/mc2/src/test/java/listeners/TestAllureListener.java
@@ -0,0 +1,55 @@
+package listeners;
+
+import driverfactory.MC2DriverFactory;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestAllureListener implements ITestListener {
+
+    @Attachment(type = "image/png")
+    protected byte[] screenshot(WebDriver driver) {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        screenshot(MC2DriverFactory.getPortalDriver());
+        MC2DriverFactory.closePortalBrowser();
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        screenshot(MC2DriverFactory.getPortalDriver());
+        MC2DriverFactory.closePortalBrowser();
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        screenshot(MC2DriverFactory.getPortalDriver());
+        MC2DriverFactory.closePortalBrowser();
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        screenshot(MC2DriverFactory.getPortalDriver());
+        MC2DriverFactory.closePortalBrowser();
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+    }
+}
