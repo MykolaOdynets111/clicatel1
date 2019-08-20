9f56009186cbbffa8c7d4e1ca95fdd0d6485989e
tarasmytlovych <tarasmytlovych@gmail.com>
2019-08-20 19:05:02 +0300

    Added new twitter page for qa

--- web_automation/shared/src/main/java/socialaccounts/TwitterUsers.java
+++ web_automation/shared/src/main/java/socialaccounts/TwitterUsers.java
@@ -4,9 +4,10 @@ package socialaccounts;
 public enum TwitterUsers {
 
 //    FIRST_USER("Click", "qa2", "click_qa2", "Qwerty123=", "@click_qa2", "", "1120976215819608064", "");
-    FIRST_USER("Tom", "Smith", "generalbankaqa@gmail.com", "p@$$w0rd4te$t", "@tomaqa0", "p@$$w0rd4te$t", "979311039996157952", ""),
-    TOUCHGO_USER("Tom", "", "", "", "@tomaqa0", "p@$$w0rd4te$t", "979311039996157952", "");
+    FIRST_USER("Tom", "Smith", "generalbankaqa@gmail.com", "p@$$w0rd4te$t", "@tomaqa0", "p@$$w0rd4te$t", "979311039996157952", "", "+380 50 5083236"),
+    TOUCHGO_USER("Tom", "", "", "", "@tomaqa0", "p@$$w0rd4te$t", "979311039996157952", "", ""),
 
+    QA_PAGE_OWNER("Automation", "Owner", "startertgplan@gmail.com", "p@$$w0rd4te$t", "@AutomationOwner", "p@$$w0rd4te$t", "979311039996157952", "", "+380 974070813");
 
     String userName;
     String userSurname;
@@ -16,8 +17,9 @@ public enum TwitterUsers {
     String userMailBoxPassword;
     String dmUserId;
     String tweetUserId;
+    String userPhone;
 
-   TwitterUsers(String userName, String userSurname, String userEmail, String userPass, String screenName, String userMailBoxPassword, String dmUserId, String tweetUserId) {
+   TwitterUsers(String userName, String userSurname, String userEmail, String userPass, String screenName, String userMailBoxPassword, String dmUserId, String tweetUserId, String userPhone) {
         this.userName = userName;
         this.userSurname = userSurname;
         this.userEmail = userEmail;
@@ -26,6 +28,7 @@ public enum TwitterUsers {
         this.userMailBoxPassword = userMailBoxPassword;
         this.dmUserId = dmUserId;
         this.tweetUserId = tweetUserId;
+        this.userPhone = userPhone;
     }
 
     private static TwitterUsers LOGGED_IN_USER = null;
@@ -71,4 +74,6 @@ public enum TwitterUsers {
     public String getTweetUserId() {
         return tweetUserId;
     }
+
+    public String getUserPhone() {return userPhone;}
 }
