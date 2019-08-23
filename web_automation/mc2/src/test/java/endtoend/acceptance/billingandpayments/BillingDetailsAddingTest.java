c27a4c7a4ac29af8a408d618a8feb826dad99790
tarasmytlovych <tarasmytlovych@gmail.com>
2019-08-23 17:41:28 +0300

    Updated waits

--- web_automation/mc2/src/test/java/endtoend/acceptance/billingandpayments/BillingDetailsAddingTest.java
+++ web_automation/mc2/src/test/java/endtoend/acceptance/billingandpayments/BillingDetailsAddingTest.java
@@ -78,7 +78,7 @@ public class BillingDetailsAddingTest extends APICreatedAccountTest  {
     private void changeZipCode(){
         billingDetailsPage.getBillingContactsDetails().enterPostalCode("79032")
                                                         .clickSaveChanges();
-        billingDetailsPage.waitWhileProcessing(2,2);
+        billingDetailsPage.waitWhileProcessing(1,1);
 
         soft.assertEquals(billingDetailsPage.getNotificationAlertText(),
                 "Your changes have been saved successfully",
