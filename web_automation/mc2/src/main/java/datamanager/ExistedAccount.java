7295509223e003995baff83fd4f4edf1e4553c18
tarasmytlovych <tarasmytlovych@gmail.com>
2019-08-29 13:31:08 +0300

    mc2: Added stage account for billing

--- web_automation/mc2/src/main/java/datamanager/ExistedAccount.java
+++ web_automation/mc2/src/main/java/datamanager/ExistedAccount.java
@@ -13,12 +13,13 @@ public enum ExistedAccount {
     TESTING_BILLING("aqa_veniam789", "Greenfelder, Wiegand and Feil","aqatest_1566304859863@gmail.com", "p@$$w0rd4te$t", "testing", "Donald Spinka", "Donald", "Spinka", "ff8080816c952f26016caf0b82c60305", "starter"),
     DEMO_BILLING("aqa_et840", "The best bank", "aqatest_1566568879666@gmail.com", "p@$$w0rd4te$t", "demo", "Clement Kuvalis", "Clement", "Kuvalis", "ff8080816cbeacc1016cbec8287d0028", "starter"),
     QA_BILLING("aqa_molestiae796", "Quigley, Lubowitz and Wiegand", "aqatest_1566305636483@gmail.com", "p@$$w0rd4te$t", "qa", "Jorge Kertzmann", "Jorge", "Kertzmann", "ff8080816caab0fb016caf1758e00010", "starter"),
-    STAGE_BILLING("aqa_consequatur533", "Botsford-Klein", "aqatest_1567062503916@gmail.com", "p@$$w0rd4te$t", "stage", "Luther Veum", "Luther", "Veum", "ff8080816cd3c445016cdc343d21011c", "starter"),
+    STAGE_BILLING("aqa_consequuntur173", "Sunflower", "automationmc2+1567072279616@gmail.com", "newp@ssw0rd", "stage", "Tyler Bogisich", "Tyler", "Bogisich", "ff8080816cd3c2c0016cdcc9768900df", "starter"),
 
 
     TESTING_PAYMENT("aqa_nemo943", "The best bank","aqatest_1565876438838@gmail.com", "p@$$w0rd4te$t", "testing", "Twanda Hettinger", "Twanda", "Hettinger", "ff8080816c952f26016c958268140074", "starter"),
     DEMO_PAYMENT("aqa_nesciunt597", "Lakin, Friesen and Green", "aqatest_1566568141992@gmail.com", "p@$$w0rd4te$t", "demo", "Fabian Kihn", "Fabian", "Kihn", "ff8080816cbeacc1016cbebcdd13000f", "standard"),
     QA_PAYMENT("aqa_corporis032", "Schiller, Prohaska and Nikolaus", "aqatest_1565853411462@gmail.com", "p@$$w0rd4te$t", "qa", "Adolfo Rowe", "Adolfo", "Rowe", "ff8080816c912129016c9422f3cb0014", "starter"),
+    STAGE_PAYMENT("aqa_consequatur533", "Botsford-Klein", "aqatest_1567062503916@gmail.com", "p@$$w0rd4te$t", "stage", "Luther Veum", "Luther", "Veum", "ff8080816cd3c445016cdc343d21011c", "starter"),
 
     TESTING_EMAIL("aqa_tempora107", "","automationmc2+1565961000111@gmail.com", "p@$$w0rd4te$t", "testing", "Sudie Lakin", "Sudie", "Lakin", "", "starter"),
 
