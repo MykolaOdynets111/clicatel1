3911d6415b91322fe95b584bb0913d6cd5e407e2
tarasmytlovych <tarasmytlovych@gmail.com>
2019-09-02 14:15:27 +0300

    Updated twitter Customer 360 test

--- web_automation/touch/src/main/java/apihelper/ApiHelper.java
+++ web_automation/touch/src/main/java/apihelper/ApiHelper.java
@@ -602,7 +602,7 @@ public class ApiHelper implements DateTimeHelper, VerificationHelper {
             fullName = respJSON.getString("personalDetails.firstName") + " " + lastName;
         }
         String location = respJSON.getString("personalDetails.location") == null ? "Unknown location" : respJSON.getString("personalDetails.location");
-
+        if(location.isEmpty()) location = "Unknown location";
         String customerSince = getCustomerSince(respJSON);
 
         String channelUsername = "";
@@ -610,12 +610,12 @@ public class ApiHelper implements DateTimeHelper, VerificationHelper {
             channelUsername = respJSON.getString("personalDetails.channelUsername").isEmpty() ? "Unknown" : respJSON.getString("personalDetails.channelUsername");
         }catch (NullPointerException e){
             channelUsername = respJSON.getString("personalDetails.channelUsername") == null ? "Unknown" : respJSON.getString("personalDetails.channelUsername");
-
         }
+        if(channelUsername.contains("_")) channelUsername = "@" + channelUsername;
         String phone =  (respJSON.getString("clientProfiles.attributes.phone[0]")==null || respJSON.getString("clientProfiles.attributes.phone[0]").isEmpty()) ? "Unknown" : respJSON.getString("clientProfiles.attributes.phone[0]");
         String email = (respJSON.getString("personalDetails.email")==null || respJSON.getString("personalDetails.email").isEmpty()) ? "Unknown" : respJSON.getString("personalDetails.email");
 
-        return new Customer360PersonalInfo(fullName, location,
+        return new Customer360PersonalInfo(fullName.trim(), location,
                 "Customer since: " + customerSince, email,
                 channelUsername, phone.replaceAll(" ", "") );
     }
