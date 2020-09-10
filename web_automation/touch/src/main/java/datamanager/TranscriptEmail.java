695c3fcf35ec4500da4ed52775e81ed08b0c2ee1
Serhii Ocheretnyi <serhii.ocheretnyi@perfectial.com>
2020-09-10 11:54:57 +0300

    updated qa transcript mail

--- web_automation/touch/src/main/java/datamanager/TranscriptEmail.java
+++ web_automation/touch/src/main/java/datamanager/TranscriptEmail.java
@@ -5,8 +5,9 @@ import drivermanager.ConfigManager;
 import java.util.Arrays;
 
 public enum TranscriptEmail {
-
-    QA_TRANSCRIPT("standarttouchgoplan@gmail.com", "p@$$w0rd4te$t", "qa"),
+//todo revert mail after twitter unblock the account
+ //   QA_TRANSCRIPT("standarttouchgoplan@gmail.com", "p@$$w0rd4te$t", "qa"),
+    QA_TRANSCRIPT("touchdemotenant@gmail.com", "12345qwer", "qa"),
     DEMO_TRANSCRIPT("aqamc2771@gmail.com", "p@$$w0rd4te$t", "demo"),
     DEV_TRANSCRIPT("updateplan@gmail.com", "p@$$w0rd4te$t", "dev"),
     INTEGRATION_TRANSCRIPT("touchdemotenant@gmail.com", "12345qwer", "integration"),
