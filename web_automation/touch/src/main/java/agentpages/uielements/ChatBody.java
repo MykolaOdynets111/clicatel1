b88ad749ba9234860e561f3b09d303f6e6a757ac
isas <ihor.sas@perfectial.com>
2020-11-13 10:37:04 +0200

    rewrite checking message in chat

--- web_automation/touch/src/main/java/agentpages/uielements/ChatBody.java
+++ web_automation/touch/src/main/java/agentpages/uielements/ChatBody.java
@@ -4,10 +4,14 @@ import abstractclasses.AbstractUIElement;
 import apihelper.ApiHelper;
 import datamanager.Tenants;
 import org.apache.commons.lang3.Range;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Rectangle;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.Color;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
+
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
@@ -54,10 +58,10 @@ public class ChatBody extends AbstractUIElement {
     private WebElement rateCard;
 
     @FindBy(css = ".rate-input label.checked input")
-    private  WebElement selectedRate;
+    private WebElement selectedRate;
 
     @FindBy(css = ".rate-card-feedback-text")
-    private  WebElement rateCardComment;
+    private WebElement rateCardComment;
 
     @FindBy(css = ".from.file-msg")
     private WebElement attachmentMessage;
@@ -74,41 +78,41 @@ public class ChatBody extends AbstractUIElement {
                     .filter(e1 -> e1.getMessageText().equals(messageText))
                     .findFirst().get();
             return theMessage.getWrappedElement();
-        } catch (NoSuchElementException e){
-            Assert.fail("There is no such user message: "+messageText+"");
+        } catch (NoSuchElementException e) {
+            Assert.fail("There is no such user message: " + messageText + "");
             return null;
         }
     }
 
-    public boolean isAttachmentMessageShown(){
+    public boolean isAttachmentMessageShown() {
         return wheelScrollDownAndIsDisplayed(this.getCurrentDriver(),
                 this.getCurrentDriver().findElement(By.cssSelector(scrollElement)),
                 attachmentMessage, 3);
-       // return isElementShown(this.getCurrentDriver(), attachmentMessage, 3);
+        // return isElementShown(this.getCurrentDriver(), attachmentMessage, 3);
     }
 
-    public ChatAttachment getAttachmentFile(){
-        return  new ChatAttachment(attachmentMessage).setCurrentDriver(this.getCurrentDriver());
+    public ChatAttachment getAttachmentFile() {
+        return new ChatAttachment(attachmentMessage).setCurrentDriver(this.getCurrentDriver());
     }
 
-    public String getPersonalInfoText(){
+    public String getPersonalInfoText() {
         return getTextFromElem(this.getCurrentDriver(), userInfoCard, 2, "Personal Info Card");
     }
 
     public boolean isUserMessageShown(String usrMessage) {
         try {
             waitForElementToBeVisibleByCss(this.getCurrentDriver(), scrollElement, 5);
-        } catch(TimeoutException e){
+        } catch (TimeoutException e) {
             Assert.fail("Chat body is not visible");
         }
         waitForAppearAndDisappear(this.getCurrentDriver(), spinner, 1, 5);
         String locator = String.format(fromUserMessagesXPATH, usrMessage);
         waitForElementToBePresentByXpath(this.getCurrentDriver(), locator, 8);
         WebElement message = this.getCurrentDriver().findElement(By.xpath(locator));
-        if (isElementShownByXpath(this.getCurrentDriver(), locator, 3 )){
+        if (isElementShownByXpath(this.getCurrentDriver(), locator, 3)) {
             return true;
         }
-        return wheelScrollUpAndIsDisplayed(this.getCurrentDriver(), this.getCurrentDriver().findElement(By.cssSelector(scrollElement)),  message);
+        return wheelScrollUpAndIsDisplayed(this.getCurrentDriver(), this.getCurrentDriver().findElement(By.cssSelector(scrollElement)), message);
     }
 
     public boolean isMoreThanOneUserMassageShown() {
@@ -126,59 +130,67 @@ public class ChatBody extends AbstractUIElement {
                 .getAgentResponseEmoji();
     }
 
-    public boolean isToUserMessageShown(String userMessage){
-        for(WebElement message: toUserMessages){
-            if(message.getText().trim().contains(userMessage))return true;
+    public boolean isToUserMessageShown(String userMessage) {
+        for (int i = toUserMessages.size() - 1; i >= 0; i--) {
+            wheelScrollUpToElement(this.getCurrentDriver(),
+                    this.getCurrentDriver().findElement(By.cssSelector(scrollElement)),
+                    toUserMessages.get(i), 1);
+
+            if (toUserMessages.get(i).getText().trim().contains(userMessage)) {
+                wheelScroll(this.getCurrentDriver(),
+                        this.getCurrentDriver().findElement(By.cssSelector(scrollElement)),
+                        2000, 0, 0);
+                return true;
+            }
         }
         return false;
     }
 
-    public List<String> getAllMessages(){
+    public List<String> getAllMessages() {
         waitForElementsToBePresentByXpath(this.getCurrentDriver(), messagesInChatBodyXPATH, 5);
         return findElemsByXPATH(this.getCurrentDriver(), messagesInChatBodyXPATH)
-                    .stream().map(e -> new AgentDeskChatMessage(e).setCurrentDriver(this.getCurrentDriver()))
-                    .map(e -> e.getMessageInfo().replace("\n", " "))
-                    .collect(Collectors.toList());
+                .stream().map(e -> new AgentDeskChatMessage(e).setCurrentDriver(this.getCurrentDriver()))
+                .map(e -> e.getMessageInfo().replace("\n", " "))
+                .collect(Collectors.toList());
     }
 
-    public boolean isOTPDividerDisplayed(){
+    public boolean isOTPDividerDisplayed() {
         if (otpDividersBlocks.size() > 0) {
             return isElementShown(this.getCurrentDriver(), otpDividersBlocks.get(otpDividersBlocks.size() - 1), 3);
-        }
-        else
+        } else
             return false;
     }
 
-    public String getLastOTPCode(){
+    public String getLastOTPCode() {
         return getTextFromElem(this.getCurrentDriver(), lastOTPDividerBlock, 2, "Last OTP divider block")
                 .substring(4, 8);
     }
 
-    public boolean isNewOTPCodeDifferent(){
-        String lastCode = getTextFromElem(this.getCurrentDriver(), (otpDividersBlocks.get(otpDividersBlocks.size()-1)),
+    public boolean isNewOTPCodeDifferent() {
+        String lastCode = getTextFromElem(this.getCurrentDriver(), (otpDividersBlocks.get(otpDividersBlocks.size() - 1)),
                 3, "");
-        String previousCode = getTextFromElem(this.getCurrentDriver(), (otpDividersBlocks.get(otpDividersBlocks.size()-2)),
+        String previousCode = getTextFromElem(this.getCurrentDriver(), (otpDividersBlocks.get(otpDividersBlocks.size() - 2)),
                 3, "");
         return !lastCode.equals(previousCode);
     }
 
-    public boolean verifyMessagesPosition(){
+    public boolean verifyMessagesPosition() {
         Rectangle rect = this.getWrappedElement().getRect();
-        int chatBodyCenter = rect.getX() + rect.getWidth()/2;
+        int chatBodyCenter = rect.getX() + rect.getWidth() / 2;
         boolean agentMessageRight = toUserMessages.stream()
-                .allMatch(e -> (e.getRect().getX()+e.getRect().getWidth()) > chatBodyCenter);
-        boolean chatMessageLeft = fromUserMessages .stream()
+                .allMatch(e -> (e.getRect().getX() + e.getRect().getWidth()) > chatBodyCenter);
+        boolean chatMessageLeft = fromUserMessages.stream()
                 .allMatch(e -> e.getLocation().getX() < chatBodyCenter);
 
         return agentMessageRight && chatMessageLeft;
     }
 
-    public boolean verifyAgentMessageColours(){
+    public boolean verifyAgentMessageColours() {
         String primaryHex = ApiHelper.getTenantInfo(Tenants.getTenantUnderTestOrgName())
                 .getBody().jsonPath().getString("tenantProperties.value[0]");
-        String primaryColorRgb = Color.fromString("#"+primaryHex).asRgb();
+        String primaryColorRgb = Color.fromString("#" + primaryHex).asRgb();
         int expRrbSum = calculateRgbSum(primaryColorRgb);
-        Range<Integer> expRange = Range.between(expRrbSum-2, expRrbSum+2);
+        Range<Integer> expRange = Range.between(expRrbSum - 2, expRrbSum + 2);
 
         boolean agentMessageColor = toUserMessages.stream()
                 .map(e -> Color.fromString(e.getCssValue("background-color")).asRgb())
@@ -191,46 +203,45 @@ public class ChatBody extends AbstractUIElement {
         return agentMessageColor && agentIconColor;
     }
 
-    private int calculateRgbSum(String rgb){
+    private int calculateRgbSum(String rgb) {
         return Arrays.stream(rgb.split("\\(")[1].replace(")", "").split(","))
                 .map(String::trim).mapToInt(Integer::parseInt).sum();
     }
 
     public boolean isValidDefaultUserProfileIcon() {
-        File image = new File(System.getProperty("user.dir")+"/touch/src/test/resources/profileicons/user_default.png");
-        Boolean isValidIcon =  isWebElementEqualsImage(this.getCurrentDriver(), userProfileIcon, image);
-        if (!isValidIcon){
-            File image2 = new File(System.getProperty("user.dir")+"/touch/src/test/resources/profileicons/user_default_with_line.png");
-            isValidIcon =  isWebElementEqualsImage(this.getCurrentDriver(), userProfileIcon, image2);
+        File image = new File(System.getProperty("user.dir") + "/touch/src/test/resources/profileicons/user_default.png");
+        Boolean isValidIcon = isWebElementEqualsImage(this.getCurrentDriver(), userProfileIcon, image);
+        if (!isValidIcon) {
+            File image2 = new File(System.getProperty("user.dir") + "/touch/src/test/resources/profileicons/user_default_with_line.png");
+            isValidIcon = isWebElementEqualsImage(this.getCurrentDriver(), userProfileIcon, image2);
         }
         return isValidIcon;
     }
 
     public boolean isValidAgentAvatarIsShown() {
-        File image = new File(System.getProperty("user.dir")+"/touch/src/test/resources/agentphoto/chatAgentIcon.png");
-        Boolean isValidIcon =  isWebElementEqualsImage(this.getCurrentDriver(), agentImage, image);
+        File image = new File(System.getProperty("user.dir") + "/touch/src/test/resources/agentphoto/chatAgentIcon.png");
+        Boolean isValidIcon = isWebElementEqualsImage(this.getCurrentDriver(), agentImage, image);
         return isValidIcon;
     }
 
 
-
-    public boolean isRateCardShown(){
+    public boolean isRateCardShown() {
         return isElementShown(this.getCurrentDriver(), rateCard, 3);
     }
 
-    public String getRate(){
+    public String getRate() {
         return selectedRate.getAttribute("value");
     }
 
-    public String getRateCardComment(){
-       return getTextFromElem(this.getCurrentDriver(), rateCardComment, 1, "Comment in rate Card");
+    public String getRateCardComment() {
+        return getTextFromElem(this.getCurrentDriver(), rateCardComment, 1, "Comment in rate Card");
     }
 
-    public boolean isStopCardShown(){
+    public boolean isStopCardShown() {
         return isElementShown(this.getCurrentDriver(), stopCard, 3);
     }
 
-    public String getStopCardText(){
+    public String getStopCardText() {
         return getTextFromElem(this.getCurrentDriver(), stopCardText, 1, "Stop Card text").trim();
     }
 
