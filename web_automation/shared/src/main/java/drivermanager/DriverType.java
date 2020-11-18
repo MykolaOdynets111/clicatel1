92810f66139cd569221b669a3d605684b3991f1c
Serhii Ocheretnyi <serhii.ocheretnyi@perfectial.com>
2020-11-18 14:15:10 +0200

    firefox run implementation is added

--- web_automation/shared/src/main/java/drivermanager/DriverType.java
+++ web_automation/shared/src/main/java/drivermanager/DriverType.java
@@ -1,12 +1,15 @@
 package drivermanager;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.FirefoxDriverManager;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.remote.CapabilityType;
@@ -98,6 +101,42 @@ public enum DriverType {
         public WebDriver getWebDriverObject(MutableCapabilities capabilities) {
             ChromeDriverManager.getInstance().setup();
             return new ChromeDriver((ChromeOptions) capabilities);
+        }
+
+        @Override
+        protected boolean isKnownAs(String name) {
+            return knownNames.contains(name);
+        }
+    },
+
+    FIREFOX {
+        private final List<String> knownNames = Arrays.asList("firefox", "mozila");
+
+        @Override
+        public MutableCapabilities getDesiredCapabilities() {
+            LoggingPreferences logPrefs = new LoggingPreferences();
+            logPrefs.enable(LogType.BROWSER, Level.ALL);
+            logPrefs.enable(LogType.PERFORMANCE, Level.ALL);
+
+            FirefoxOptions options = new FirefoxOptions();
+//            options.setLegacy(true);
+            options.setCapability("profile.default_content_setting_values.cookies", 1);
+            options.setCapability("profile.block_third_party_cookies", false);
+
+            options.setCapability(CapabilityType.SUPPORTS_APPLICATION_CACHE, false);
+            options.setCapability(CapabilityType.LOGGING_PREFS, logPrefs);
+            if(ConfigManager.isRemote()) {
+                options.addArguments("--width=1360");
+                options.addArguments("--height=1020");
+                options.setHeadless(true);
+            }
+            return options;
+        }
+
+
+        public WebDriver getWebDriverObject(MutableCapabilities capabilities) {
+            FirefoxDriverManager.getInstance().setup();
+            return new FirefoxDriver((FirefoxOptions) capabilities);
         }
 
         @Override
