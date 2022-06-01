a25bdd6c93002ffed20c780bb58d26c7b571c44b
socheretnyi <serhii.ochretnyi@perfectial.com>
2022-06-01 15:49:02 +0300

    gradle update to 7.4.2 version

--- touch/src/main/java/agentpages/dashboard/DashboardSettingsPage.java
+++ touch/src/main/java/agentpages/dashboard/DashboardSettingsPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.FindBys;
 import portalpages.PortalAbstractPage;
 
 public class DashboardSettingsPage extends PortalAbstractPage {
