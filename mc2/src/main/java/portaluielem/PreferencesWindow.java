d330452730cc7713b41d4e2eae95b942644e1f0e
bitta.rana <bitta.rana@clickatell.com>
2022-10-25 15:36:41 -0400

    CCD-2076 modified on comments

--- mc2/src/main/java/portaluielem/PreferencesWindow.java
+++ mc2/src/main/java/portaluielem/PreferencesWindow.java
@@ -100,7 +100,6 @@ public class PreferencesWindow extends BasePortalWindow {
 
     public String errorMessageShown(){
         return pendingErrorMessageDecimalNumbers.getText();
-        //Assert.assertEquals(pendingErrorMessageDecimalNumbers.getText(),decimalErrorMessage);
     }
 
 
