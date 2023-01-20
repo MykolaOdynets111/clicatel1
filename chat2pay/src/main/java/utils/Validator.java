ba6e35ec62059d52bca6801166ea91a1ff7eb216
kateryna.burla <kateryna.burla@avenga.com>
2023-01-20 10:53:23 +0200

    C2P-4433 fixes

--- chat2pay/src/main/java/utils/Validator.java
+++ chat2pay/src/main/java/utils/Validator.java
@@ -3,14 +3,16 @@ package utils;
 import api.models.response.failedresponse.ErrorResponse;
 import datamodelclasses.validateobjects.ErrorValidatorObject;
 import io.restassured.response.Response;
+import org.assertj.core.api.SoftAssertions;
 
 import java.util.Map;
 
 import static java.lang.String.format;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.testng.Assert.assertTrue;
 
 public class Validator {
+
+    private static SoftAssertions softly = new SoftAssertions();
 
     public static void validateErrorResponse(Response response, Map<String, String> data) {
         ErrorValidatorObject errorData = new ErrorValidatorObject(data);
@@ -18,12 +20,13 @@ public class Validator {
         String error = errorResponse.getMessage();
         String expectedCode = data.get("o.responseCode");
 
-        assertThat(response.statusCode())
+        softly.assertThat(response.statusCode())
                 .as(format("Status code is not equals to %s", expectedCode))
                 .isEqualTo(Integer.valueOf(expectedCode));
-        assertTrue(error.contains(errorData.getErrorMessage()),
-                "Error message is incorrect or not returned /n" +
-                        "Error from server:" + error);
+        softly.assertThat(error)
+                .as(format("Error message is incorrect. Error from server: %s", error))
+                .contains(errorData.getErrorMessage());
+        softly.assertAll();
     }
 
     public static void checkResponseCode(Response response, String code) {
