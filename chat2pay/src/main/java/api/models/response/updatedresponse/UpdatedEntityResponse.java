dbff365cd8ddc2b2bb10c770b26f299734290e60
kateryna.burla <kateryna.burla@avenga.com>
2023-02-15 16:04:39 +0200

    C2P-4721

--- chat2pay/src/main/java/api/models/response/updatedresponse/UpdatedEntityResponse.java
+++ chat2pay/src/main/java/api/models/response/updatedresponse/UpdatedEntityResponse.java
@@ -1,4 +1,4 @@
-package api.models.response.updateEntityResponse;
+package api.models.response.updatedresponse;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
