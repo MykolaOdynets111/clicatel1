4aa78456441453830310cf68bc28eda9c5913f1e
kateryna.burla <kateryna.burla@avenga.com>
2023-02-24 16:36:27 +0200

    C2P-4756 fixes

--- chat2pay/src/main/java/steps/PaymentConfigurationSupportEndpointsSteps.java
+++ chat2pay/src/main/java/steps/PaymentConfigurationSupportEndpointsSteps.java
@@ -1,13 +1,18 @@
 package steps;
 
 import api.clients.ApiHelperChat2Pay;
+import api.models.response.merchantbillinginfo.State;
 import api.models.response.paymentgatewaysettingsresponse.BillingType;
 import api.models.response.paymentgatewaysettingsresponse.CardNetwork;
 import api.models.response.paymentgatewaysettingsresponse.Country;
 import api.models.response.paymentgatewaysettingsresponse.Currency;
 import api.models.response.paymentgatewaysettingsresponse.IntegrationType;
+import api.models.response.paymentgatewaysettingsresponse.PaymentType;
+import api.models.response.paymentgatewaysettingsresponse.TransactionType;
 import io.cucumber.java.en.Then;
 import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
+import org.openqa.selenium.NoSuchSessionException;
 
 import java.util.Map;
 import java.util.NoSuchElementException;
@@ -18,8 +23,12 @@ import static api.clients.ApiHelperPaymentConfigurationSupport.getCountry;
 import static api.clients.ApiHelperPaymentConfigurationSupport.getCurrency;
 import static api.clients.ApiHelperPaymentConfigurationSupport.getIntegrationType;
 import static api.clients.ApiHelperPaymentConfigurationSupport.getLocale;
+import static api.clients.ApiHelperPaymentConfigurationSupport.getPaymentType;
+import static api.clients.ApiHelperPaymentConfigurationSupport.getState;
+import static api.clients.ApiHelperPaymentConfigurationSupport.getTransactionType;
 import static api.clients.ApiHelperPayments.getPaymentGatewaySettingsResponse;
 import static java.lang.Integer.parseInt;
+import static java.lang.String.format;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class PaymentConfigurationSupportEndpointsSteps extends GeneralSteps {
@@ -105,6 +114,53 @@ public class PaymentConfigurationSupportEndpointsSteps extends GeneralSteps {
                     .stream().filter(bt -> bt.getId() == parseInt(dataMap.get("o.id")))
                     .findFirst().ifPresent(currency ->
                             assertThat(currency.getName()).isEqualToIgnoringCase(dataMap.get("o.name")));
+        }
+    }
+
+    @Then("^User gets 'Payment Type'$")
+    public void getPaymentTypes(Map<String, String> dataMap) {
+        response = getPaymentType(TOKEN);
+
+        if (getResponseCode(dataMap) == 200) {
+            PaymentType paymentType = response.jsonPath().getList("", PaymentType.class)
+                    .stream().filter(bt -> bt.getId() == parseInt(dataMap.get("o.id")))
+                    .findFirst().orElseThrow(NoSuchSessionException::new);
+            softly.assertThat(paymentType.getName()).isEqualToIgnoringCase(dataMap.get("o.name"));
+            softly.assertThat(paymentType.getDescription()).isEqualToIgnoringCase(dataMap.get("o.description"));
+            softly.assertThat(paymentType.getDisplayName()).isEqualToIgnoringCase(dataMap.get("o.displayName"));
+            softly.assertAll();
+        } else {
+            Assertions.fail(format("The response code is not as expected %s", getResponseCode(dataMap)));
+        }
+    }
+
+    @Then("^User gets 'State'$")
+    public void getStates(Map<String, String> dataMap) {
+        response = getState(TOKEN);
+
+        if (getResponseCode(dataMap) == 200) {
+            State state = response.jsonPath().getList("", State.class)
+                    .stream().filter(bt -> bt.getId() == parseInt(dataMap.get("o.id")))
+                    .findFirst().orElseThrow(NoSuchSessionException::new);
+
+            assertThat(state.getName()).isEqualToIgnoringCase(dataMap.get("o.name"));
+        } else {
+            Assertions.fail(format("The response code is not as expected %s", getResponseCode(dataMap)));
+        }
+    }
+
+    @Then("^User gets 'Transaction Type'$")
+    public void getTransactionTypes(Map<String, String> dataMap) {
+        response = getTransactionType(TOKEN);
+
+        if (getResponseCode(dataMap) == 200) {
+            TransactionType state = response.jsonPath().getList("", TransactionType.class)
+                    .stream().filter(bt -> bt.getId() == parseInt(dataMap.get("o.id")))
+                    .findFirst().orElseThrow(NoSuchSessionException::new);
+
+            assertThat(state.getName()).isEqualToIgnoringCase(dataMap.get("o.name"));
+        } else {
+            Assertions.fail(format("The response code is not as expected %s", getResponseCode(dataMap)));
         }
     }
 }
