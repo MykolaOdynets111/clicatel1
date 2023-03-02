aebe6758e1bbceb8a8c8c9122cd864397acfce26
kateryna.burla <kateryna.burla@avenga.com>
2023-03-02 18:34:34 +0200

    C2P-4758 updated version logic

--- chat2pay/src/main/java/api/clients/Endpoints.java
+++ chat2pay/src/main/java/api/clients/Endpoints.java
@@ -1,18 +1,22 @@
 package api.clients;
 
+import static java.lang.String.format;
+
 public class Endpoints {
 
     public static final String C2P_URL_DEMO = "https://demo-chat2pay-service.int-eks-dev.shared-dev.eu-west-1.aws.clickatell.com";
+    public static final String API_VERSION = "v2";
+    public static final String C2P_URL_WITH_VERSION = C2P_URL_DEMO + "/" + API_VERSION;
 
     // Widgets
-    public static final String WIDGETS_ENDPOINT = C2P_URL_DEMO + "/v2/widget";
+    public static final String WIDGETS_ENDPOINT = C2P_URL_WITH_VERSION + "/widget";
     public static final String EXISTED_WIDGETS_ENDPOINT = WIDGETS_ENDPOINT + "/all?detailed=false&page=0&size=50";
     public static final String WIDGET_INTEGRATION_ENDPOINT = WIDGETS_ENDPOINT + "/%s/integration";
     public static final String WIDGET_SHOWED_LINKED_API_ENDPOINT = WIDGETS_ENDPOINT + "/%s/show-linked-api";
 
     // Account Settings
-    public static final String ACCOUNT_SETTINGS = C2P_URL_DEMO + "/v2/account/settings";
-    public static final String ACCOUNT_SETTINGS_SHOW_TUTORIAL = C2P_URL_DEMO + "/v2/account/settings/show-tutorial";
+    public static final String ACCOUNT_SETTINGS = C2P_URL_WITH_VERSION + "/account/settings";
+    public static final String ACCOUNT_SETTINGS_SHOW_TUTORIAL = C2P_URL_WITH_VERSION + "/account/settings/show-tutorial";
 
     // API Key Management
     public static final String WIDGET_API_KEYS_ENDPOINT = WIDGETS_ENDPOINT + "/%s/api-keys";
@@ -23,7 +27,7 @@ public class Endpoints {
     public static final String CHANNEL_STATUS = WIDGETS_ENDPOINT + "/%s/status";
 
     // Configurations
-    public static final String CHAT_TO_PAY_CONFIGURATION_ENDPOINT = C2P_URL_DEMO + "/api/v2/config";
+    public static final String CHAT_TO_PAY_CONFIGURATION_ENDPOINT = C2P_URL_DEMO + format("/api/%s/config", API_VERSION);
 
     // Message Configurations
     public static final String MESSAGE_CONFIGURATIONS = WIDGETS_ENDPOINT + "/%s/message-configurations";
@@ -44,14 +48,13 @@ public class Endpoints {
 
     // Payments
     public static final String PAYMENTS_GATEWAY_ENDPOINT = WIDGETS_ENDPOINT + "/%s/payment-gateway-settings";
-    public static final String CHAT_TO_PAY_ENDPOINT = C2P_URL_DEMO + "/api/v2/chat-2-pay";
-    public static final String CANCEL_PAYMENT_LINK_ENDPOINT = C2P_URL_DEMO + "/api/v2/cancel/";
-    public static final String PAYMENT_RECEIPT = C2P_URL_DEMO + "/api/v2/order-receipt";
+    public static final String CHAT_TO_PAY_ENDPOINT = C2P_URL_DEMO + format("/api/%s/chat-2-pay", API_VERSION);
+    public static final String CANCEL_PAYMENT_LINK_ENDPOINT = C2P_URL_DEMO + format("/api/%s/cancel/", API_VERSION);
+    public static final String PAYMENT_RECEIPT = C2P_URL_DEMO + format("/api/%s/order-receipt", API_VERSION);
 
     // Two Way Numbers
     public static final String TWO_WAY_NUMBER = WIDGETS_ENDPOINT + "/%s/two-way-numbers";
 
     //Merchant's Billing Info
     public static final String MERCHANTS_BILLING_INFO = WIDGETS_ENDPOINT + "/%s/merchant-billing-info";
-
 }
