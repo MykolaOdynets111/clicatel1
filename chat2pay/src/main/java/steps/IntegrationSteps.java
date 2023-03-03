456e60c37aa1f6acbac0cf913d56ed452b0316b0
modynets <modynets@roku.com>
2023-03-03 15:25:01 +0200

    C2P-4737

--- chat2pay/src/main/java/steps/IntegrationSteps.java
+++ chat2pay/src/main/java/steps/IntegrationSteps.java
@@ -5,22 +5,24 @@ import io.cucumber.java.en.Then;
 import io.restassured.response.Response;
 
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 import static api.clients.ApiHelperIntegration.getApplicationResponse;
 import static api.clients.ApiHelperIntegration.getIntegrationResponse;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static utils.Validator.validateErrorResponse;
 
 public class IntegrationSteps extends GeneralSteps {
 
     @Then("^User gets integration information for widget$")
     public void getIntegrationConfiguration(Map<String, String> dataMap) {
-        String widgetId = getWidgetId(dataMap);
-        Response response = getIntegrationResponse(widgetId);
+        Response response = getIntegrationResponse(getWidgetId(dataMap));
         int expectedResponseCode = getResponseCode(dataMap);
         if (expectedResponseCode == 200) {
-            response.jsonPath().getList("", IntegrationResponse.class)
-                    .forEach(k -> checkIntegrationRsponse(dataMap, k));
+            IntegrationResponse integrationResponse = response.jsonPath().getList("", IntegrationResponse.class)
+                    .stream().filter(ir -> ir.getIntegrator().getName().equals(dataMap.get("o.name")))
+                    .findFirst()
+                    .orElseThrow(NoSuchElementException::new);
+            checkIntegrationResponse(dataMap, integrationResponse);
         } else if (expectedResponseCode == 404) {
             validateErrorResponse(response, dataMap);
         }
@@ -28,22 +30,21 @@ public class IntegrationSteps extends GeneralSteps {
 
     @Then("^User gets details about application$")
     public void getApplicationDetails(Map<String, String> dataMap) {
-        String widgetId = getWidgetId(dataMap);
         String applicationId = dataMap.get("i.applicationId");
-        Response response = getApplicationResponse(widgetId, applicationId);
+        Response response = getApplicationResponse(getWidgetId(dataMap), applicationId);
         int expectedResponseCode = getResponseCode(dataMap);
         if (expectedResponseCode == 200) {
             IntegrationResponse integrationResponse = response.as(IntegrationResponse.class);
-            checkIntegrationRsponse(dataMap, integrationResponse);
+            checkIntegrationResponse(dataMap, integrationResponse);
         } else if (expectedResponseCode == 404) {
             validateErrorResponse(response, dataMap);
         }
     }
 
-    private void checkIntegrationRsponse(Map<String, String> dataMap, IntegrationResponse k) {
-        softly.assertThat(k.getIntegrator()).isNotNull();
-        softly.assertThat(k.getNotificationUrls()).isNotNull();
-        softly.assertThat(k.getIntegrationStatus()).isEqualTo(dataMap.get("o.integrationStatus"));
+    private void checkIntegrationResponse(Map<String, String> dataMap, IntegrationResponse integrationResponse) {
+        softly.assertThat(integrationResponse.getIntegrator().getType()).isEqualTo(dataMap.get("o.integratorType"));
+        softly.assertThat(integrationResponse.getNotificationUrls().getPaymentStatusNotification()).isEqualTo(dataMap.get("o.paymentStatusNotification"));
+        softly.assertThat(integrationResponse.getIntegrationStatus()).isEqualTo(dataMap.get("o.integrationStatus"));
         softly.assertAll();
     }
 }
