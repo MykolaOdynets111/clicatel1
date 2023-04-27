f3e6029d4b516f69880947429ed7f9653d58a40c
CLICKATELL\umair.hussain <umair.hussain@clickatell.com>
2023-04-27 12:35:29 -0400

    CCH-94 Allure reports added

--- chathub/src/test/java/runner/homepage/RunChatHubTests.java
+++ chathub/src/test/java/runner/homepage/RunChatHubTests.java
@@ -3,15 +3,16 @@ package runner.homepage;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-
+@Test()
 @CucumberOptions(
         plugin={"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
         monochrome = true,
-        features ={"src/test/java/scenario/APIs/ProviderApis.feature"},
-        glue = {"chathub/src/main/java/steps/"}
-        )
-public class RunChatHubTest extends AbstractTestNGCucumberTests {
+        features ={"chathub/src/test/java/scenario/APIs"},
+        glue = {"steps"}
+)
+public class RunChatHubTests extends AbstractTestNGCucumberTests {
 
     @Override
     @DataProvider(parallel = true)
